779c1e135d8ef8930e1285a69e65a982155b062d
sofijasljusar <sofija.sljusar@gmail.com>
2025-11-04 19:16:21 +0200

    Change PayingQueueGroupModel: add name field and automatically generate code

--- whopays/models.py
+++ whopays/models.py
@@ -1,13 +1,31 @@
+import string
+import random
 from django.db import models
 from django.contrib.auth.models import User
 
+GROUP_CODE_LENGTH = 8
+
+
+def generate_group_code(length=GROUP_CODE_LENGTH):
+    chars = string.ascii_uppercase + string.digits
+    return ''.join(random.choices(chars, k=length))
+
 
 class PayingQueueGroup(models.Model):
-    code = models.CharField(max_length=10, unique=True)
+    code = models.CharField(max_length=10, unique=True, blank=True)
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        if not self.code:
+            new_code = generate_group_code()
+            while PayingQueueGroup.objects.filter(code=new_code).exists():
+                new_code = generate_group_code()
+            self.code = new_code
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"Paying Queue Group with code: {self.code}."
+        return f"Paying Queue Group '{self.name}' with code: {self.code}."
 
 
 class GroupMember(models.Model):
