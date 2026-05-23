d3ee59e65f2934962e7ae9d9baa8da254a3d7468
sofijasljusar <sofija.sljusar@gmail.com>
2026-05-23 18:50:00 +0300

    Switch to Postgres in dev (so can test concurrency). Run django locally to mirror prod. Remove sqlite3.

--- referee/settings.py
+++ referee/settings.py
@@ -64,19 +64,11 @@ WSGI_APPLICATION = 'referee.wsgi.application'
 
 ASGI_APPLICATION = "referee.asgi.application"
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.environ["DATABASE_URL"]
 
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+}
 
 
 # Password validation
