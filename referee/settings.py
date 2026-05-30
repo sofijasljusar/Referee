36d949fc71fdb7d3e9a5660e177dad729613a4fe
sofijasljusar <sofija.sljusar@gmail.com>
2026-05-30 16:33:07 +0300

    Add default database (used in deployed app without extra services).

--- referee/settings.py
+++ referee/settings.py
@@ -64,11 +64,19 @@ WSGI_APPLICATION = 'referee.wsgi.application'
 
 ASGI_APPLICATION = "referee.asgi.application"
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASES = {
-    "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
-}
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
